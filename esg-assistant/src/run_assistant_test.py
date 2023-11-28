826a8820ba123676d5dc9c3efbbb76e1db30213f
PVSaiHarshith <vp82@njit.edu>
2023-11-28 12:27:01 -0500

    updated code and questions to create 3 different csv for each question ie cause areas, demographics, impact areas

--- esg-assistant/src/run_assistant_test.py
+++ esg-assistant/src/run_assistant_test.py
@@ -1,4 +1,3 @@
-# Updated script to replace %s with each company name and fill a CSV file with company, question, and response
 import os
 from ibm_watson import AssistantV2
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
@@ -23,27 +22,27 @@ assistant.set_service_url(service_url)
 session = assistant.create_session(assistant_id=assistant_id).get_result()
 session_id = session['session_id']
 
-# Define the input and output files
+# Define the input files
 questions_file = 'questions.txt'
 companies_file = 'companies.txt'
-output_file = 'qa_output.csv'  # Use CSV format
 
 # Read the list of companies from companies.txt
 with open(companies_file, 'r') as companies_file:
     companies = [line.strip() for line in companies_file if line.strip()]
 
-# Open the output CSV file for writing
-with open(output_file, 'w', newline='') as csv_file:
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['Company', 'Question', 'Response'])  # Write the header
+# Read questions from the input file and generate answers for each company
+with open(questions_file, 'r') as questions_file:
+    for question_template in questions_file:
+        question_template = question_template.strip()
+        if question_template:
+            # Create a new CSV file for each question
+            output_file = f'qa_output_{question_template.replace(" ", "_")}.csv'
+            with open(output_file, 'w', newline='') as csv_file:
+                csv_writer = csv.writer(csv_file)
+                csv_writer.writerow(['Company', 'Question', 'Response'])  # Write the header
 
-    # Read questions from the input file and generate answers for each company
-    with open(questions_file, 'r') as questions_file:
-        for question_template in questions_file:
-            question_template = question_template.strip()
-            if question_template:
                 for company in companies:
-                    print(f"processing {company}")
+                    print(f"processing {company} for question: {question_template}")
                     question = question_template.replace('%s', company)
                     response = assistant.message(
                         assistant_id=assistant_id,
@@ -57,7 +56,7 @@ with open(output_file, 'w', newline='') as csv_file:
                     response_text = response['output']['generic'][0]['text']
                     csv_writer.writerow([company, question, response_text])  # Write company, question, and answer to CSV
 
+            print(f'Answers for "{question_template}" saved to {output_file}')
+
 # Delete the session when done
 assistant.delete_session(assistant_id=assistant_id, session_id=session_id)
-
-print(f'Company, questions, and answers saved to {output_file}')
